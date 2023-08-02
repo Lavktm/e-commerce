b2ad80b85d822c408bb1582ce79df68cfd57f3e6
lavktm <lav.ktm@gmail.com>
2023-08-02 22:30:13 +0530

    routing added

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,38 @@
+import prismadb from "@/lib/primadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+    children,
+    params
+}: {
+    children: React.ReactNode;
+    params: { storeId: string }
+}) {
+    //checking user is logged in or not
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect('/sign-in')
+    }
+    //loading store created by the user
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    })
+
+    if (!store) {
+        redirect('/')
+    }
+
+    return (
+        <>
+            <div>This will be a Navbar</div>
+            {children}
+        </>
+    )
+
+}
+
