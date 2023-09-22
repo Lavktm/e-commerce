cc41914488f21b53c7c25181b1c0029e4ff19828
lavktm <lav.ktm@gmail.com>
2023-09-22 17:58:22 +0530

    Change in api getProduct

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -14,6 +14,9 @@ export async function GET(req: Request,
             where: {
                 id: params.categoryId,
             },
+            include: {
+                billboard: true
+            }
         })
         return NextResponse.json(category);
     }
